6cab3a035092ed2114d2a1323d8ac0745ef54335
chakreshram <chakreshram11@gmail.com>
2025-01-22 22:26:33 +0530

    first

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='main-home'>
+        <h1>Home PageHome PageHome PageHome PageHome PageHome PageHome PageHome PageHome Page</h1>
+
+    </div>
+  )
+}
+
+export default Home
